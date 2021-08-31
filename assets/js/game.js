7e7cfccda954b2139ef2b21addb8691bdb646eac
daniel natale <dnatale667@gmail.com>
2021-08-31 12:08:57 -0400

    fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,47 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "dumb Idiot Loser";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+var fight = function(){
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+
+enemyHealth = enemyHealth - playerAttack; 
+window.alert(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+playerHealth = playerHealth - enemyAttack;
+console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+    }
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+        window.alert(playerName + " has chosen to skip the fight!");
+
+      }
+      
+      
+      else {
+        window.alert("You need to choose a valid option. Try again!");
+        fight();
+      }
+
+};
+
+fight();
