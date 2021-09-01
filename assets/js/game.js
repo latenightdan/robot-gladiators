c7f2400baf10fdfd846d2478727c56735744d74b
daniel natale <dnatale667@gmail.com>
2021-09-01 12:53:01 -0400

    end start

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,9 +11,29 @@ var enemyNames = ["trash", "uncool dork", "scumbag"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+//todo: add start and end game functions. in end game alert player of stats, ask if they want to play again. if yes, start game. 
+//after skip or defeat, ask player if they want to shop. if not, continue as normal. if yes, call shop function. 
+//in shop ask player if they want to refil health, upgrade, or leave the shop
+//refill subract money points from player to increase health
+//if leave, alert goodbye and exit
+//if invalid option, call shop again
 
-
-
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+      } 
+      else {
+        window.alert("You've lost your robot in battle.");
+      }
+      var playAgainConfirm = window.confirm("you done?");
+      if(playAgainConfirm){
+          startGame();
+      }
+      else{
+          window.alert("bye bye bye");
+      }
+  };
 var fight = function(enemyName) { 
      
     while(enemyHealth > 0 && playerHealth >0) {
@@ -55,7 +75,13 @@ break;
 }
 }; 
 
+var startGame = function(){
 for (var i = 0; i < enemyNames.length; i++) {
+    // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
       if(playerHealth > 0){
        window.alert("welcome to the fuckin goon squad" + (i+1));
             var pickedEnemyName = enemyNames[i];
@@ -66,6 +92,10 @@ for (var i = 0; i < enemyNames.length; i++) {
             window.alert("you died");
         }
     }
+    endGame();
+};
+startGame();
+
 
 
 
